6466abd7eb6bd6af89072e815e7e7ff7b4f43dcc
Lindsay Young <lindsayn.young@gsa.gov>
2023-09-27 15:52:53 -0400

    pinning Django

--- setup.py
+++ setup.py
@@ -2,46 +2,46 @@ import os
 
 from setuptools import find_packages, setup
 
-with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+with open(os.path.join(os.path.dirname(__file__), "README.md")) as readme:
     README = readme.read()
 
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
-    name='ReVal',
-    version='v0.7.0',
+    name="ReVal",
+    version="v0.7.0",
     packages=find_packages(),
     include_package_data=True,
-    license='CC0-1.0',
-    description='Django app to upload, validate, review, and accept data files',
+    license="CC0-1.0",
+    description="Django app to upload, validate, review, and accept data files",
     long_description=README,
-    url='https://github.com/18F/ReVAL',
-    author='18F',
-    author_email='data-federation@gsa.gov',
+    url="https://github.com/18F/ReVAL",
+    author="18F",
+    author_email="data-federation@gsa.gov",
     classifiers=[
-        'Environment :: Web Environment',
-        'Framework :: Django',
-        'Framework :: Django :: 2.2',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',  # example license
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Topic :: Internet :: WWW/HTTP',
-        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
-        'License :: Public Domain',
-        'License :: CC0 1.0 Universal (CC0 1.0) Public Domain Dedication'
+        "Environment :: Web Environment",
+        "Framework :: Django",
+        "Framework :: Django :: 3.2",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",  # example license
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.9",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+        "License :: Public Domain",
+        "License :: CC0 1.0 Universal (CC0 1.0) Public Domain Dedication",
     ],
     install_requires=[
-        'django>=2.0,<3.0',
-        'djangorestframework',
-        'dj-database-url',
-        'goodtables',
-        'json_logic_qubit',
-        'psycopg2-binary',
-        'pyyaml',
-        'requests',
+        # todo: make this a range
+        "django==3.2.21",
+        "djangorestframework",
+        "dj-database-url",
+        "goodtables",
+        "json_logic_qubit",
+        "psycopg2-binary",
+        "pyyaml",
+        "requests",
     ],
 )
